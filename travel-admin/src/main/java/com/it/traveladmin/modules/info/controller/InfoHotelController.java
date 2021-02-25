54a93c80911772961facd716325f40ee07ff84b4
Administrator <2330715018@qq.com>
2021-02-25 21:34:53 +0800

    项目初始化框架

---
+++ travel-admin/src/main/java/com/it/traveladmin/modules/info/controller/InfoHotelController.java
@@ -0,0 +1,21 @@
+package com.it.traveladmin.modules.info.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 酒店数据表 前端控制器
+ * </p>
+ *  说明
+ * @author SJG
+ * @since 2021-02-25
+ */
+@RestController
+@RequestMapping("/info/infoHotel")
+public class InfoHotelController {
+
+}
+
